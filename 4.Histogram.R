d4cc7d9a871217d921c5d9f661a2118b49ba1a51
Shaikmuzamil9 <124116755+Shaikmuzamil9@users.noreply.github.com>
2024-09-23 09:56:10 +0530

    Create 4.Histogram.R

---
+++ 4.Histogram.R
@@ -0,0 +1,8 @@
+age <- c(25, 30, 35, 40, 45, 50, 55, 60, 65, 70)
+hist(age, 
+     main = "Age Distribution",
+     xlab = "Age", 
+     ylab = "Frequency",
+     col = "lightgreen", 
+     border = "black",
+     breaks = 4)  # Number of bins

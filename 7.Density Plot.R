51622b7bbc572919a6f1d3aac6af537b09bd5376
Shaikmuzamil9 <124116755+Shaikmuzamil9@users.noreply.github.com>
2024-09-23 09:58:28 +0530

    Create 7.Density Plot.R

---
+++ 7.Density Plot.R
@@ -0,0 +1,6 @@
+weights <- c(60, 65, 70, 75, 80, 85, 90, 95, 100)
+plot(density(weights), 
+     main = "Density Plot of Weights",
+     xlab = "Weight (kg)", 
+     ylab = "Density",
+     col = "blue")
